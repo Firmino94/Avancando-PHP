758e58b64995f1ca441872f496212cbcef08d3d2
Guilherme <guilhermes017@gmail.com>
2023-04-17 20:02:45 -0300

    Separando Função

--- transacoes-bancarias.php
+++ transacoes-bancarias.php
@@ -1,35 +1,6 @@
 <?php
 
-function exibeMensagem($mensagem)
-{
-    echo $mensagem . PHP_EOL;
-}
-
-
-function sacar(array $conta, float $valorASacar): array
-{
-    if ($valorASacar > $conta['saldo']) {
-        exibeMensagem("Você não pode sacar este valor");
-    } else {
-        $conta['saldo'] -= $valorASacar;
-    }
-
-    return $conta;
-}
-
-
-function depositar(array $conta, float $valorADepositar): array 
-{
-    if ($valorADepositar > 0) {
-        $conta['saldo'] += $valorADepositar;
-    } else {
-        exibeMensagem("Depósitos precisam ser positivos");
-    }
-
-    return $conta;
-}
-
-
+include 'funcoes.php';
 
 $contasCorrentes = [
     '123.456.789-10' => [
@@ -67,8 +38,9 @@ $contasCorrentes['123.456.789-11'] = sacar(
 
 
 foreach ($contasCorrentes as $cpf => $conta) {
-    exibeMensagem
-    ($cpf . " " . $conta['titular'] . ' ' . $conta['saldo']);
+    exibeMensagem(
+        "$cpf {$conta['titular']} {$conta['saldo']}"
+    );
 }
 
 
